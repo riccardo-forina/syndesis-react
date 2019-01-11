929ee646b32f9485935c7bf6a196fc41c6f47b2f
Riccardo Forina <rforina@redhat.com>
2019-01-11 12:03:43 +0100

    Further steps towards typed routing

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -2,4 +2,5 @@ export * from './getConnectionIcon';
 export * from './key';
 export * from './WithListViewToolbarHelpers';
 export * from './WithLoader';
+export * from './WithRouteData';
 export * from './WithRouter';
